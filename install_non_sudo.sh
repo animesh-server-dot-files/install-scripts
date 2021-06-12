ca47d5382454e42f1f7f29c1dfa1443f538efde4
Animesh <animesh.workplace@gmail.com>
2021-06-12 15:56:42 +0530

    added hard links and commented build of usher

--- install_non_sudo.sh
+++ install_non_sudo.sh
@@ -20,7 +20,18 @@ cd $MODULE_PREFIX
 echo "Completed"
 
 echo "Downloading all required modules"
-git clone --recursive https://github.com/animesh-server-dot-files/modules.git
+git clone --recursive https://github.com/animesh-server-dot-files/modules.git modules_source
+mkdir modules/python
+mkdir modules/golang
+mkdir modules/nextstrain
+mkdir modules/usher
+mkdir modules/anaconda
+ln modules_source/python/3.9.5 modules/python/3.9.5
+ln modules_source/python/2.7.18 modules/python/2.7.18
+ln modules_source/golang/1.16.4 modules/golang/1.16.4
+ln modules_source/nextstrain/1.0.0_a9 modules/nextstrain/1.0.0_a9
+ln modules_source/usher/0.3 modules/usher/0.3
+ln modules_source/anaconda/3-2021.05 modules/anaconda/3-2021.05
 . $MODULE_PREFIX/environment_modules/init/bash
 echo "Completed"
 
@@ -30,16 +41,16 @@ go get -u -ldflags="-s -w" github.com/gokcehan/lf
 go get github.com/cov-ert/gofasta
 echo "Completed"
 
-echo "Building UShER"
-cd modules/usher/v0.3/source
-mkdir -p usher_build && cd usher_build
-cmake -DTBB_DIR=${PWD}/../oneTBB-2019_U9  -DCMAKE_PREFIX_PATH=${PWD}/../oneTBB-2019_U9/cmake ..
-make -j 20
-mkdir -p ../../package
-cp parsimony.pb.h parsimony.pb.cc matOptimize usher matUtils ../../package/
-cd ../..
-rm -rf source/usher_build source/oneTBB-2019_U9/cmake/TBBConfig.cmake source/oneTBB-2019_U9/cmake/TBBConfigVersion.cmake
-echo "Completed"
+# echo "Building UShER"
+# cd modules/usher/v0.3/source
+# mkdir -p usher_build && cd usher_build
+# cmake -DTBB_DIR=${PWD}/../oneTBB-2019_U9  -DCMAKE_PREFIX_PATH=${PWD}/../oneTBB-2019_U9/cmake ..
+# make -j 20
+# mkdir -p ../../package
+# cp parsimony.pb.h parsimony.pb.cc matOptimize usher matUtils ../../package/
+# cd ../..
+# rm -rf source/usher_build source/oneTBB-2019_U9/cmake/TBBConfig.cmake source/oneTBB-2019_U9/cmake/TBBConfigVersion.cmake
+# echo "Completed"
 
 echo "Configuring Anaconda and Installing required packages"
 module load anaconda/3-2021.05
